b8f0cdf21efa86fa1bdd36e744503aacbb973a50
Maga <abdulloev156380@gmail.com>
2024-06-06 17:54:09 +0500

    add autoloader

--- includes/autoloader.php
+++ includes/autoloader.php
@@ -2,9 +2,14 @@
 
 spl_autoload_register('MyAutoload');
 
-function MyAutoload($classname) {
-    $path = __DIR__ . 'classes';
+function MyAutoload($classname)
+{
+    $path = "classes/";
     $extenction = ".php";
     $fullPath = $path . $classname . $extenction;
+
+    if (!file_exists($fullPath)) {
+        return false;
+    }
     include_once $fullPath;
 }
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
 include_once ("includes/autoloader.php");
+
+$user = new User(1, 'mahmadullo', 'mahmadullo@gmail.com', '21313');
+
+print_r($user);
